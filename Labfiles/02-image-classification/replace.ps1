726ff05aa921129ef3aa3f455430903c2c2da475
Teerasej Jiraphatchandej <teerasej@gmail.com>
2025-01-20 22:50:10 +0700

    Add command to run lab file from root

--- Labfiles/02-image-classification/replace.ps1
+++ Labfiles/02-image-classification/replace.ps1
@@ -2,6 +2,9 @@
 # sudo apt-get update
 # sudo apt-get install -y powershell
 
+# run the file from root
+# pwsh Labfiles/02-image-classification/replace.ps1
+
 
 $storageAcct = '<storageAccount>'
 (Get-Content training-images/training_labels.json) -replace '<storageAccount>', $storageAcct | Out-File training-images/training_labels.json
