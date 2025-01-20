c2f88b81b68ef62aab8bdf553120e45a6ee1427c
Teerasej Jiraphatchandej <teerasej@gmail.com>
2025-01-20 22:46:33 +0700

    Add command to install powershell to linux codespace

--- Labfiles/02-image-classification/replace.ps1
+++ Labfiles/02-image-classification/replace.ps1
@@ -1,2 +1,7 @@
+# Install PowerShell on Linux Codespace
+# sudo apt-get update
+# sudo apt-get install -y powershell
+
+
 $storageAcct = '<storageAccount>'
 (Get-Content training-images/training_labels.json) -replace '<storageAccount>', $storageAcct | Out-File training-images/training_labels.json
